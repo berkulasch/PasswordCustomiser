164cde91c9dcbc8fdd45fb3f959d62db71e8d882
ulasberk <85496179+berkulasch@users.noreply.github.com>
2024-05-22 16:55:39 +0200

    Initial Commit

---
+++ Customiser/CustomiserApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomiserApp.swift
+//  Customiser
+//
+//  Created by Berk Ulas on 22.05.24.
+//
+
+import SwiftUI
+
+@main
+struct CustomiserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
